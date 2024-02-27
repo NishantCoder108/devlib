8a24e5b2a3fc664637ec435eb27e422faa3e230f
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-02-28 03:15:10 +0530

    Remove console in Production

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.tsx";
 import "./index.css";
+import { DEVMODE } from "./constants.ts";
+
+if (!DEVMODE) console.log = () => {};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <NextUIProvider>
-            {/* <main className="dark text-foreground bg-background"> */}
             <App />
-            {/* </main> */}
         </NextUIProvider>
     </React.StrictMode>
 );
