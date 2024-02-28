8b2fbd698c029ff0a825625016b07832eb677c43
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-02-29 01:21:27 +0530

    Sign out implemented

--- src/components/BooksLibrary.tsx
+++ src/components/BooksLibrary.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Button } from "@nextui-org/react";
+import { signOut } from "firebase/auth";
+import { auth } from "../configs/firebase";
+import { logoutUser } from "../features/authSlice";
+import { useAppDispatch } from "../app/hooks";
+import { useNavigate } from "react-router-dom";
 
 const BooksLibrary = () => {
-    return <div className="text-red-700">BooksLibrary</div>;
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        signOut(auth)
+            .then(() => {
+                dispatch(logoutUser());
+                navigate("/", { replace: true });
+            })
+            .catch((error) => {
+                console.log("Logout Failed Error :", error);
+            });
+    };
+    return (
+        <div className="text-red-700">
+            <p>Books library</p>
+            <Button onClick={handleLogout}> Logout</Button>
+        </div>
+    );
 };
 
 export default BooksLibrary;
