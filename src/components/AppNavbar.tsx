755dfea0282a976f99d4ce94432a2522f562d7d8
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-02-29 14:18:08 +0530

    Navbar implemented

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,76 +1,100 @@
-import React from "react";
+import Logo from "../assets/svg/logo.svg";
+import Logout from "../assets/svg/logout.svg";
+import Ai from "../assets/svg/ai.svg";
 import {
     Navbar,
     NavbarBrand,
     NavbarContent,
     NavbarItem,
-    Link,
+    Button,
     Input,
-    DropdownItem,
-    DropdownTrigger,
-    Dropdown,
-    DropdownMenu,
-    Avatar,
 } from "@nextui-org/react";
+import { signOut } from "firebase/auth";
+import { auth } from "../configs/firebase";
+import { logoutUser } from "../features/authSlice";
+import { useAppDispatch } from "../app/hooks";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface ISearchData {
+    searchText: string;
+}
 
 export default function AppNavbar() {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm<ISearchData>();
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        signOut(auth)
+            .then(() => {
+                dispatch(logoutUser());
+                navigate("/", { replace: true });
+            })
+            .catch((error) => {
+                console.log("Logout Failed Error :", error);
+            });
+    };
+
+    const handleSearchText = handleSubmit((data) => {
+        console.log("search data", data);
+        reset();
+    });
+
     return (
-        <Navbar isBordered>
-            <NavbarContent justify="start">
-                <NavbarBrand className="mr-4">
-                    Logo
-                    <p className="hidden sm:block font-bold text-inherit">
-                        ACME
-                    </p>
+        <Navbar disableAnimation maxWidth="full" className="bg-transparent">
+            <NavbarContent className="" justify="center">
+                <NavbarBrand>
+                    <img src={Logo} alt="devlib logo"></img>
                 </NavbarBrand>
-                {/* <NavbarContent className="hidden sm:flex gap-3">
-                    <NavbarItem>
-                        <Link color="foreground" href="#">
-                            Features
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem isActive>
-                        <Link href="#" aria-current="page" color="secondary">
-                            Customers
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link color="foreground" href="#">
-                            Integrations
-                        </Link>
-                    </NavbarItem>
-                </NavbarContent> */}
-                <NavbarContent className="hidden sm:flex gap-3">
-                    <Input
-                        classNames={{
-                            base: "max-w-full sm:max-w-[10rem] h-10",
-                            mainWrapper: "h-full",
-                            input: "text-small",
-                            inputWrapper:
-                                "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                        }}
-                        placeholder="Type to search..."
-                        size="sm"
-                        startContent={"ICON"}
-                        type="search"
-                    />
-                </NavbarContent>
             </NavbarContent>
 
-            <NavbarContent as="div" className="items-center" justify="center">
-                {/* <Input
-                    classNames={{
-                        base: "max-w-full sm:max-w-[10rem] h-10",
-                        mainWrapper: "h-full",
-                        input: "text-small",
-                        inputWrapper:
-                            "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                    }}
-                    placeholder="Type to search..."
-                    size="sm"
-                    startContent={"ICON"}
-                    type="search"
-                /> */}
+            <NavbarContent justify="end" className="">
+                <form
+                    onSubmit={handleSearchText}
+                    className="flex items-center rounded-none"
+                >
+                    <Input
+                        {...register("searchText", { required: true })}
+                        autoComplete="off"
+                        classNames={{
+                            base: "w-full h-10 ",
+                            mainWrapper: "h-full w-[46vw] ",
+                            input: "text-small px-2",
+                            inputWrapper:
+                                "h-full font-normal text-default-500 bg-white dark:bg-default-500/20 rounded-l-full",
+                        }}
+                        placeholder="Search your programming books with AI, e.g., Can you suggest JavaScript-related books? ..."
+                        size="sm"
+                        type="search"
+                        fullWidth={true}
+                    />
+                    <Button
+                        type="submit"
+                        // fullWidth
+                        className="rounded-r-full bg-white border border-l-1 px border-gray-50 "
+                        isIconOnly
+                    >
+                        <img src={Ai} alt="ai logo" className="w-4" />
+                    </Button>
+                </form>
+
+                <NavbarItem>
+                    <Button
+                        isIconOnly
+                        aria-label="logout"
+                        className="bg-transparent"
+                        radius="full"
+                        onClick={handleLogout}
+                    >
+                        <img src={Logout} alt="logout" className="w-4" />
+                    </Button>
+                </NavbarItem>
             </NavbarContent>
         </Navbar>
     );
