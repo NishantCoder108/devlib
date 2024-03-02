18e5b2bbec6ebc040a28b2ec96043d5b42b56b48
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-02 13:10:34 +0530

    remove unwanted code

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import AppNavbar from "./AppNavbar";
 import BooksLibrary from "./BooksLibrary";
 
 const Homepage = () => {
     const [text, setText] = useState("js");
+
     const handleSearchText = (data: string) => {
-        console.log("homepage data", data);
+        console.log("Homepage Search Text :-", data);
         setText(data);
     };
 
     return (
         <div>
-            {" "}
             <AppNavbar searchText={handleSearchText} />
             <BooksLibrary query={text} />
         </div>
