67ae27f89f7e4be1a353094efd697a47d6d84134
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-02 13:10:08 +0530

    created custom hooks

---
+++ src/configs/hooks/useSearchBookById.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { Search_BOOK_By_ID } from "../../constants";
+
+const useSearchBookById = (bookId: string) => {
+    const [bookDetails, setBookDetails] = useState([]);
+
+    const fetchBookById = async (bookId: string) => {
+        const data = await fetch(Search_BOOK_By_ID + "/" + bookId);
+        const jsonData = await data.json();
+
+        console.log("Books List :- ", jsonData);
+
+        setBookDetails(jsonData);
+    };
+
+    useEffect(() => {
+        fetchBookById(bookId);
+    }, [bookId]);
+
+    return bookDetails;
+};
+
+export default useSearchBookById;
